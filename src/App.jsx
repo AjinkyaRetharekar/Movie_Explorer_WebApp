56391f543f2e2f47cde75a2a4c7fc1746161d563
Ajinkya Retharekar <ajinkyaretharekar12345@gmail.com>
2025-11-06 10:39:35 +0530

    Intial Commit - Uploading Movie Explorer Project

---
+++ src/App.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react'
+import Search from './Components/Search.jsx'
+import Loading from './Components/Loading.jsx'
+import MovieCard from './Components/MovieCard.jsx'
+import { useDebounce } from "react-use"
+import { getTrendingMovies, updateSearchCount } from './appwrite.js'
+
+
+
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [movieList, setMovieList] = useState([])
+  const [trendingMovies, setTrendingMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 800, [searchTerm])
+
+  const fetchMovies = async (query = '') => {
+    setIsLoading(true)
+    setErrorMessage('')
+
+    try {
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+
+      const response = await fetch(endpoint, API_OPTIONS)
+
+      if (!response.ok) throw new Error('Failed to load movies')
+
+      const data = await response.json()
+
+      setMovieList(data.results || [])
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0])
+      }
+    } catch (error) {
+      setErrorMessage('Failed to fetch movies. Please try again later.')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies()
+      setTrendingMovies(movies)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(debouncedSearchTerm)
+  }, [debouncedSearchTerm])
+
+  useEffect(() => {
+    loadTrendingMovies()
+  }, [])
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero-img.png" alt="hero banner" />
+          <h1>Find <span className="text-gradient">Movies</span> you will enjoy without any hassle</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+
+        {trendingMovies?.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+            <ul>
+              {trendingMovies.map((movie, i) => (
+                <li key={movie.$id}>
+                  <p>{i + 1}</p>
+                  <img
+                    src={movie.poster_url || "/No-Poster.png"}
+                    onError={(e) => (e.target.src = "/No-Poster.png")}
+                    alt={movie.title}
+                  />
+
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {isLoading ? (
+            <Loading />
+          ) : errorMessage ? (
+            <p style={{ color: "red" }}>{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Components/Loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="loader-container">
+      <div className="spinner"></div>
+      <span className="sr-only-text">Loading...</span>
+    </div>
+  );
+}
